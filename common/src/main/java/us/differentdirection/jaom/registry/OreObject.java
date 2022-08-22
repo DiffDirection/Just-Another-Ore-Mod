da13250468ecdeace94cb31b4a712c633ff67174
RuKira <jduesling13@gmail.com>
2022-08-22 04:12:06 -0400

    Minor Changes

--- common/src/main/java/us/differentdirection/jaom/registry/OreObject.java
+++ common/src/main/java/us/differentdirection/jaom/registry/OreObject.java
@@ -4,6 +4,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import us.differentdirection.jaom.JAOreMod;
 
 public class OreObject {
     private final MiningLevel miningLevel;
@@ -18,10 +19,10 @@ public class OreObject {
     public OreObject(String name, BlockBehaviour.Properties blockProperties, Item.Properties itemProperties, Boolean hasNugget, MiningLevel miningLevel) {
         this.NAME = name;
         this.hasNugget = hasNugget;
-        this.INGOT = new Item(itemProperties);
+        this.INGOT = new Item(itemProperties.tab(JARegistry.JAOM_TAB)); // TOBEDECIDED
         if (hasNugget)
-            this.NUGGET = new Item(itemProperties);
-        this.RAW = new Item(itemProperties);
+            this.NUGGET = new Item(itemProperties.tab(JARegistry.JAOM_TAB)); // TOBEDECIDED
+        this.RAW = new Item(itemProperties.tab(JARegistry.JAOM_TAB)); // TOBEDECIDED
         this.ORE = new Block(blockProperties.sound(SoundType.STONE));
         this.STORAGE = new Block(blockProperties.sound(SoundType.METAL));
 
